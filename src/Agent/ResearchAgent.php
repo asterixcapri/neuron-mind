6e6ebc8e13815853166ccb3491dd45cdd37298d6
Alessandro Astarita <aleast@caprionline.it>
2025-06-14 12:34:55 +0200

    Added TavilySearchTool

--- src/Agent/ResearchAgent.php
+++ src/Agent/ResearchAgent.php
@@ -5,6 +5,7 @@ namespace NeuronMind\Agent;
 use NeuronAI\Agent;
 use NeuronAI\Providers\AIProviderInterface;
 use NeuronAI\Providers\OpenAI\OpenAI;
+use NeuronAI\Tools\Toolkits\Tavily\TavilySearchTool;
 
 class ResearchAgent extends Agent
 {
@@ -14,5 +15,14 @@ class ResearchAgent extends Agent
             key: $_ENV['OPENAI_API_KEY'],
             model: 'gpt-4o-mini'
         );
+    }
+
+    protected function tools(): array
+    {
+        return [
+            TavilySearchTool::make(
+                key: $_ENV['TAVILY_API_KEY']
+            ),
+        ];
     }
 }
